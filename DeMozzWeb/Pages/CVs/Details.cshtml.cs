b0aaf9ebd257f0a8b5171e77bea70b5ac4ec09e0
YousofAhmad0 <yousofahmad81@gmail.com>
2022-08-04 01:16:42 +0300

    Done

--- DeMozzWeb/Pages/CVs/Details.cshtml.cs
+++ DeMozzWeb/Pages/CVs/Details.cshtml.cs
@@ -3,7 +3,12 @@ using DeMozzWeb.Model;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using SelectPdf;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Drawing;
+using System.IO;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
 
 namespace DeMozzWeb.Pages.CVs
 {
@@ -28,31 +33,33 @@ namespace DeMozzWeb.Pages.CVs
             Skills = CV.Skills.Split(',').ToList();
         }
 
-        //public IActionResult GeneratePdf(string html)
-        //{
-        //    html = html.Replace("StrTag", "<").Replace("EndTag", ">");
-
-        //    HtmlToPdf htmlToPdf = new HtmlToPdf();
-        //    PdfDocument pdfDocument = htmlToPdf.ConvertHtmlString(html);
-
-        //    byte[] pdf = pdfDocument.Save();
-        //    pdfDocument.Close();
-
-        //    string pdfname = CV.FN + "_" + CV.LN + ".pdf";
-        //    return File(
-        //        pdf,
-        //        "application/pdf",
-        //        pdfname
-        //        );
-        //}
-        public IActionResult OnPostGeneratePdf(string html)
+        public async Task<IActionResult> OnPost(int? id)
         {
-            HtmlToPdf oHtmlToPdf = new HtmlToPdf();
-            PdfDocument oPdfDocument = oHtmlToPdf.ConvertHtmlString(html);
-            byte[] pdf = oPdfDocument.Save();
-            oPdfDocument.Close();
-            return File(pdf, "application/pdf", CV.FN + "_" + CV.LN + ".pdf");
+            CV = await _db.CV.FirstOrDefaultAsync(m => m.Id == id);
+
+            PdfDocument document = new PdfDocument();
+            PdfPage page = document.Pages.Add();
+            PdfGraphics graphics = page.Graphics;
+            PdfFont font = new PdfStandardFont(PdfFontFamily.Helvetica, 20);
+
+            graphics.DrawString("First Name : " + CV.FN, font, PdfBrushes.Black, new PointF(0, 0));
+            graphics.DrawString("Last Name : " + CV.LN, font, PdfBrushes.Black, new PointF(0, 50));
+            graphics.DrawString("Email : " + CV.Email, font, PdfBrushes.Black, new PointF(0, 100));
+            graphics.DrawString("Gender : " + CV.Gender, font, PdfBrushes.Black, new PointF(0, 150));
+            graphics.DrawString("Nationality : " + CV.Nationality.ToString(), font, PdfBrushes.Black, new PointF(0, 200));
+            graphics.DrawString("Date : " + CV.DateOfBirth, font, PdfBrushes.Black, new PointF(0, 250));
+            graphics.DrawString("Skills : " + CV.Skills, font, PdfBrushes.Black, new PointF(0, 300));
+            graphics.DrawString("Grade : " + CV.Grade, font, PdfBrushes.Black, new PointF(0, 350));
+
+            MemoryStream stream = new MemoryStream();
+            document.Save(stream);
+            stream.Position = 0;
+            string contentType = "application/pdf";
+            string fileName = CV.FN+" "+CV.LN+".pdf";
+            return File(stream, contentType, fileName);
         }
+      
+        
 
 
     }
