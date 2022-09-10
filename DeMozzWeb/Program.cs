542a7ba05c2c1db65917eb5da3bd94c29b099c79
yousofahmad0 <yousofahmad81@gmail.com>
2022-09-10 15:53:31 +0300

    Reverting to "Done"

--- DeMozzWeb/Program.cs
+++ DeMozzWeb/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container
 builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 builder.Services.AddDbContext<AuthDBContext>(options => options.UseSqlServer(
     builder.Configuration.GetConnectionString("DefaultConnection")
